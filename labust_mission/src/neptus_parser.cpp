9bbcaf5b2605c19a3f189d8ecf390ce23a84c26b
gibo1990 <filip.mandic@gmail.com>
2015-05-11 13:29:09 +0200

    Fixed bug

--- labust_mission/src/neptus_parser.cpp
+++ labust_mission/src/neptus_parser.cpp
@@ -450,9 +450,9 @@ void startParseCallback(ros::Publisher &pubStartDispatcher, const misc_msgs::Sta
 	}
 
 	NP.underactuated = msg->underactuated;
-	NP.speed = msg->speed;
+	NP.speed = msg->speed; // read from .nmis file
 	NP.heading = msg->heading;
-	NP.heading= msg->victory_radius;
+	NP.victory_radius= msg->victory_radius;
 
 	/*** If Neptus file is successfully parsed  ***/
 	if(NP.parseNeptus(msg->fileName) == 1){
