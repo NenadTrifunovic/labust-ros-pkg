86a5cbf031143e27f71e124d7011b09a1a25f892
PlaDyPos <labust-unizg@gmail.com>
2015-05-30 20:49:35 +0200

    Updated the LF controller.

--- labust_control/src/line_following/UALFControl.cpp
+++ labust_control/src/line_following/UALFControl.cpp
@@ -163,13 +163,7 @@ namespace labust
 						con.track = out(1);
 						PIFF_ffStep(&con,Ts,0);
 						double ul = ref.body_velocity.x;
-<<<<<<< HEAD
-						if (con.windup) ul = out(0);
-=======
-
 						//if (con.windup) ul = out(0);
-
->>>>>>> 622be9516a356dc8f6e5349de2ced5537b97f4f7
 						double vl = con.output;
 						ROS_DEBUG("Command output: ul=%f, vl=%f", ul, vl);
 
@@ -206,14 +200,9 @@ namespace labust
 				nh.param("ualf_controller/sampling",Ts,Ts);
 				nh.param("velocity_controller/use_ground_vel", use_gvel, use_gvel);
 				ph.param("underactuated",underactuated,underactuated);
-<<<<<<< HEAD
 				
 				dh_pub = nh.advertise<geometry_msgs::Vector3Stamped>("dh_calc",1);
 				
-=======
-				dh_pub = nh.advertise<geometry_msgs::Vector3Stamped>("dh_calc",1);
-
->>>>>>> 622be9516a356dc8f6e5349de2ced5537b97f4f7
 				PIDBase_init(&con);
 
 				disable_axis[0] = 0;
