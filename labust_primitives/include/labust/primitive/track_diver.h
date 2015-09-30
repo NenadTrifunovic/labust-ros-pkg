2164f8919abc121f43bb640588ece439828cc729
Đula Nađ <dula.nad@gmail.com>
2015-09-30 18:52:24 +0200

    Added the primitive and virtual target controller intial skeleton.

---
+++ labust_primitives/include/labust/primitive/track_diver.h
@@ -0,0 +1,186 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2015, LABUST, UNIZG-FER
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the LABUST nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+#ifndef PRIMITIVES_TRACK_DIVER_H
+#define PRIMITIVES_TRACK_DIVER_H
+#include <labust/primitive/PrimitiveBase.hpp>
+#include <labust/math/NumberManipulation.hpp>
+
+#include <navcon_msgs/TrackDiverAction.h>
+#include <auv_msgs/NavSts.h>
+#include <auv_msgs/FSPathInfo.h>
+#include <geometry_msgs/PointStamped.h>
+#include <geometry_msgs/TwistStamped.h>
+#include <std_msgs/Float32.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <ros/ros.h>
+
+
+#include <boost/thread/mutex.hpp>
+#include <limits>
+
+namespace labust
+{
+	namespace primitive
+	{
+		/**
+		 * The class implements the diver tracking primitive utilizing the virtual target
+		 * path following.
+		 */
+		class TrackDiver : protected ExecutorBase<navcon_msgs::TrackDiverAction>
+		{
+			///Convenience typedefs
+			typedef navcon_msgs::TrackDiverGoal Goal;
+			typedef navcon_msgs::TrackDiverResult Result;
+			typedef navcon_msgs::TrackDiverFeedback Feedback;
+			typedef ExecutorBase<navcon_msgs::TrackDiverAction> Base;
+
+			///Controller enumerator
+			enum {vt = 0, numcnt};
+
+			///Minimal safety radius
+			const float min_radius;
+
+		public:
+			///Main constructor
+			TrackDiver();
+
+			///Primitive initialization
+			void init();
+
+			///Main action startup method
+			void onGoal();
+
+			///Main action preemption handler
+			void onPreempt();
+
+		protected:
+			///Helper function for controller updates
+			void updateControllers(bool on = false);
+			///Helper function for goal processing
+			void processGoal();
+			///Helper function for stopping
+			void stop();
+			///Helper function for parameter loading
+			void boundedParam(ros::NodeHandle& ph, const std::string& name, double* param, double bound, double def);
+
+			///Vehicle state handling method
+			void onStateHat(const auv_msgs::NavSts::ConstPtr& estimate);
+			///Guidance point handling method
+			void onGuidancePoint(const geometry_msgs::PointStamped::ConstPtr& point)
+			{
+				boost::mutex::scoped_lock l(goal_mux);
+				cgoal->guidance_target = point->point;
+				//Enable guidance
+				cgoal->guidance_enable = true;
+			}
+			///Variable radius handling method
+			void onRadius(const std_msgs::Float32::ConstPtr& radius)
+			{
+				boost::mutex::scoped_lock l(goal_mux);
+				cgoal->radius = labust::math::coerce(
+						radius->data, min_radius, std::numeric_limits<double>::quiet_NaN());
+			}
+			///Diver position update handling
+			void onDiverState(const auv_msgs::NavSts::ConstPtr& diver_state)
+			{
+				boost::mutex::scoped_lock l(state_mux);
+				diver_pos = *diver_state;
+			}
+			///Path speed update handling
+			void onPathSpeed(const geometry_msgs::TwistStamped::ConstPtr& dpi_r)
+			{
+				boost::mutex::scoped_lock l(state_mux);
+				this->dpi_r = dpi_r->twist.linear.x;
+			}
+
+
+			///The main worker method
+			void step();
+			///Frame updatemethod
+			void updateFS();
+			///Monitoring and guidance position caclulation
+			void setDesiredPathPosition();
+			///Heading calculation
+			void setDesiredOrientation(const auv_msgs::NavSts& estimate);
+
+			///Subscriber for the point guidance
+			ros::Subscriber guidance_sub;
+			//Subscriber for the variable radius
+			ros::Subscriber radius_sub;
+			///Path speed frame progression
+			ros::Subscriber dpi_r_sub;
+
+			///Transform frame publisher
+
+			///Diver state
+			ros::Subscriber diver_state;
+			///Current action goal
+			Goal::Ptr cgoal;
+			///New goal processing flag
+			bool process_new;
+			///Goal protection mutex
+			boost::mutex goal_mux;
+			///State protection mutex
+			boost::mutex state_mux;
+			///FS frame protector
+			boost::mutex fs_mux;
+			///Transform publisher
+			tf2_ros::TransformBroadcaster broadcaster;
+
+			///Current diver position
+			auv_msgs::NavSts diver_pos;
+			///Current desired path speed
+			double dpi_r;
+			///Current path state
+			auv_msgs::FSPathInfo path;
+			///Current feedback
+			Feedback finfo;
+
+			///Guidance FOV parameter
+			double sigma_fov;
+			///Guidance gain parameter
+			double kxi;
+			///Maximum monitoring range
+			double sigma_m;
+			//Approach gain
+			double kpsi;
+
+			///Track the unwrapped angles
+			labust::math::unwrap psi_d;
+		};
+	}
+}
+
+/* PRIMITIVES_TRACK_DIVER_H */
+#endif
---
+++ labust_primitives/src/track_diver.cpp
@@ -0,0 +1,340 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2015, LABUST, UNIZG-FER
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the LABUST nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+#include <labust/primitive/track_diver.h>
+#include <labust/tools/conversions.hpp>
+
+#include <auv_msgs/FSPathInfo.h>
+#include <navcon_msgs/EnableControl.h>
+
+using namespace labust::primitive;
+
+TrackDiver::TrackDiver():
+				Base("track_diver"),
+				min_radius(2),
+				process_new(false),
+				dpi_r(0),
+				sigma_fov(M_PI/6),
+				kxi(1.0),
+				sigma_m(10),
+				kpsi(1.0){};
+
+
+void TrackDiver::boundedParam(ros::NodeHandle& ph, const std::string& name, double* param, double bound, double def)
+{
+	ph.param(name, *param, def);
+	if (*param <= bound)
+	{
+		ROS_WARN("%s needs to be > %f.", name.c_str(),bound);
+		*param = def;
+	}
+}
+
+void TrackDiver::init()
+{
+	ros::NodeHandle nh, ph("~");
+	//Load parameters
+	double radius(7);
+	boundedParam(ph, "safety_raidus", &radius, min_radius, min_radius);
+	boundedParam(ph, "kxi", &kxi, 0.0, kxi);
+	boundedParam(ph, "kpsi", &kpsi, 0.0, kpsi);
+	boundedParam(ph, "sigma_fov", &sigma_fov, radius, sigma_fov);
+
+	//Set default curvature
+	path.curvature = 1/radius;
+	path.torsion = 0;
+	//Init subscribers
+	diver_state = nh.subscribe("diver_state",1,
+			&TrackDiver::onDiverState, this);
+	dpi_r_sub = nh.subscribe("dpi_r", 1,
+			&TrackDiver::onPathSpeed,this);
+}
+
+void TrackDiver::onGoal()
+{
+	ROS_DEBUG("TrackDiver: Goal received");
+	//Lock the state
+	boost::mutex::scoped_lock l(goal_mux);
+	bool active = !(cgoal == 0);
+	//Protect from preemption in case it is running
+	if (!active) cgoal.reset(new Goal());
+	process_new = true;
+	*cgoal = *aserver->acceptNewGoal();
+	process_new = false;
+	//Process goal
+	processGoal();
+	l.unlock();
+	//Update reference
+	this->step();
+
+	//If no goal was running
+	if (!active)
+	{
+		//Start the controllers
+		this->updateControllers(true);
+	};
+};
+
+void TrackDiver::processGoal()
+{
+	//If not empty and the topic name changed
+	if (!cgoal->guidance_topic.empty() &&
+			(cgoal->guidance_topic != guidance_sub.getTopic()))
+	{
+		ros::NodeHandle nh;
+		guidance_sub.shutdown();
+		guidance_sub =  nh.subscribe("guidance_point",1,
+				&TrackDiver::onGuidancePoint, this);
+	}
+
+	//If not empty and the topic name changed
+	if (!cgoal->radius_topic.empty() &&
+			(cgoal->radius_topic != radius_sub.getTopic()))
+	{
+		ros::NodeHandle nh;
+		radius_sub.shutdown();
+		radius_sub =  nh.subscribe("safety_radius",1,
+				&TrackDiver::onRadius, this);
+	}
+
+	if (cgoal->radius < min_radius)
+		cgoal->radius = min_radius;
+}
+
+
+void TrackDiver::onPreempt()
+{
+	ROS_DEBUG("Preempted.");
+	if (!process_new)
+	{
+		cgoal.reset();
+		this->stop();
+	}
+	else
+	{
+		ROS_DEBUG("Goal update.");
+	}
+	aserver->setPreempted();
+};
+
+void TrackDiver::onStateHat(const auv_msgs::NavSts::ConstPtr& estimate)
+{
+	//Update FS frame position internally
+	this->updateFS();
+
+	if (aserver->isActive())
+	{
+		boost::mutex::scoped_lock lg(goal_mux);
+		boost::mutex::scoped_lock ls(state_mux);
+		//Calculate desired position and orientation
+		//if on a operational circle
+		if (fabs(cgoal->vertical_offset) < cgoal->radius)
+		{
+			this->setDesiredPathPosition();
+			this->setDesiredOrientation(*estimate);
+		}
+		else
+		{
+			finfo.operational_radius = 0;
+			finfo.mu_r = 0;
+			finfo.gamma_r = 0;
+			boost::mutex::scoped_lock lf(fs_mux);
+			path.pi_tilda = 0;
+			path.xi_r = path.pi;
+			path.dxi_r = 0;
+		}
+
+		this->step();
+	}
+	else
+	{
+		boost::mutex::scoped_lock l(goal_mux);
+		if (cgoal != 0) this->stop();
+	}
+};
+
+void TrackDiver::setDesiredPathPosition()
+{
+	double nr = sqrt(cgoal->radius*cgoal->radius -
+			cgoal->vertical_offset*cgoal->vertical_offset);
+	finfo.operational_radius = nr;
+	//Find monitoring position
+	finfo.mu_r = psi_d(diver_pos.orientation.yaw);
+	if (cgoal->wrapping_enable) finfo.mu_r = labust::math::wrapRad(finfo.mu_r);
+	finfo.mu_r = nr*finfo.mu_r;
+
+	boost::mutex::scoped_lock lf(fs_mux);
+	if (cgoal->guidance_enable)
+	{
+		double dx = cgoal->guidance_target.x - diver_pos.position.north;
+		double dy = cgoal->guidance_target.y - diver_pos.position.east;
+		finfo.gamma_r = nr*atan2(dy,dx);
+
+		double diff = (finfo.gamma_r - finfo.mu_r);
+		if (cgoal->wrapping_enable) diff = nr*labust::math::wrapRad(diff/nr);
+		path.xi_r = finfo.mu_r + sigma_fov*tanh(diff/kxi);
+		if (cgoal->wrapping_enable) path.xi_r = nr*labust::math::wrapRad(path.xi_r/nr);
+		double dmu_r = nr*diver_pos.orientation_rate.yaw;
+		path.dxi_r = dmu_r + dmu_r*sigma_fov/(2*kxi)*(tanh(diff/kxi)*tanh(diff/kxi)-1);
+	}
+	else
+	{
+		finfo.gamma_r = finfo.mu_r;
+		boost::mutex::scoped_lock lf(fs_mux);
+		path.xi_r = finfo.mu_r;
+		path.dxi_r = diver_pos.orientation_rate.yaw*nr;
+	}
+	path.pi_tilda = path.pi - path.dxi_r;
+	if (cgoal->wrapping_enable) path.pi_tilda = nr*labust::math::wrapRad(path.pi_tilda);
+}
+
+void TrackDiver::setDesiredOrientation(const auv_msgs::NavSts& estimate)
+{
+	double dx = diver_pos.position.north - estimate.position.north;
+	double dy = diver_pos.position.east - estimate.position.east;
+	boost::mutex::scoped_lock lf(fs_mux);
+	path.delta_r = atan2(dy,dx);
+	path.k = 0;
+	if (cgoal->streamline_orientation)
+		path.k = 0.5*(tanh((sqrt(dx*dx+dy*dy) - sigma_m)/kpsi) + 1);
+}
+
+void TrackDiver::updateFS()
+{
+	static ros::Time last_update = ros::Time::now();
+
+	double dT = (ros::Time::now() - last_update).toSec();
+	last_update = ros::Time::now();
+
+	boost::mutex::scoped_lock l(fs_mux);
+	boost::mutex::scoped_lock lg(goal_mux);
+	//Set path info from goal if available
+	double voff = 0;
+	double radius = 1/path.curvature;
+	if (cgoal != 0)
+	{
+		voff = cgoal->vertical_offset;
+		path.curvature = 1/cgoal->radius;
+		radius = cgoal->radius;
+	}
+	lg.unlock();
+
+	//Update frame position in path coordinates
+	path.pi += dT*dpi_r;
+	//Default guidance is in the current point
+	path.pi_tilda = 0;
+	path.dxi_r = 0;
+	path.xi_r = path.pi;
+
+	if (fabs(voff) < radius)
+	{
+		double nr = sqrt(radius*radius - voff*voff);
+		path.position.x = diver_pos.position.north + nr*cos(path.pi/nr);
+		path.position.y = diver_pos.position.east + nr*sin(path.pi/nr);
+		path.position.z = diver_pos.position.depth + voff;
+		path.orientation.yaw = path.pi/nr + M_PI/2;
+	}
+	else
+	{
+		path.position.x = diver_pos.position.north;
+		path.position.y = diver_pos.position.east;
+		path.position.z = diver_pos.position.depth + voff;
+		//\todo What should be the orientation here ?
+		path.orientation.yaw = 0;
+	}
+}
+
+void TrackDiver::step()
+{
+	boost::mutex::scoped_lock l(fs_mux);
+
+	//Publish transform
+	geometry_msgs::TransformStamped tfs;
+	tfs.transform.translation.x = path.position.x;
+	tfs.transform.translation.y = path.position.y;
+	tfs.transform.translation.z = path.position.z;
+	labust::tools::quaternionFromEulerZYX(
+			path.orientation.roll,
+			path.orientation.pitch,
+			path.orientation.yaw,
+			tfs.transform.rotation);
+	tfs.child_frame_id = "sf_frame";
+	tfs.header.frame_id = "local";
+	tfs.header.stamp = ros::Time::now();
+	broadcaster.sendTransform(tfs);
+
+	//Publish reference
+	auv_msgs::FSPathInfo::Ptr piout(new auv_msgs::FSPathInfo(path));
+	piout->header.stamp = tfs.header.stamp;
+	piout->header.frame_id = "sf_frame";
+	stateRef.publish(piout);
+}
+
+void TrackDiver::stop()
+{
+	ROS_DEBUG("TrackDiver: Stopping.");
+	cgoal.reset();
+	this->updateControllers();
+	//Stop path progression
+	dpi_r = 0;
+}
+
+void TrackDiver::updateControllers(bool on)
+{
+	if (!this->control_manager.isValid())
+	{
+		ros::NodeHandle nh;
+		this->control_manager = nh.serviceClient<navcon_msgs::EnableControl>("VT_enable", true);
+	}
+	navcon_msgs::EnableControl req;
+  req.request.enable = on;
+	this->control_manager.call(req);
+}
+
+int main(int argc, char* argv[])
+{
+	ros::init(argc,argv,"track_diver");
+	labust::primitive::PrimitiveBase<
+		labust::primitive::TrackDiver,
+		auv_msgs::FSPathInfo> primitive;
+	ros::spin();
+	return 0;
+}
+
+
+
+
+
+
+
+
