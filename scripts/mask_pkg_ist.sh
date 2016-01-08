35aaf6cbdfb39f9087c97439cfb69f88ca74af52
Đula Nađ <dula.nad@gmail.com>
2016-01-08 11:18:40 +0100

    Added element to mask.

--- scripts/mask_pkg_ist.sh
+++ scripts/mask_pkg_ist.sh
@@ -5,13 +5,15 @@ SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 LABUST_LIST="ident_so labust_diagnostics labust_sim labust_allocation labust_execution labust_navigation labust_control labust_framework"
 SENSOR_LIST="labust_imu image_processing diver_net novatel_dgps sensors_util usbl tritech_sdk spatial_ins diver_net labust_kinect navquest_dvl"
 VIS_LIST="labust_gearth labust_uwsim rqt_relay usbl_uwsim labust_gui"
+DES_LIST="decision_making_examples"
 
 LABUST_PKG=${SCRIPT_DIR}/..
 SENSOR_PKG=${SCRIPT_DIR}/../../sensors-ros-pkg
 VIS_PKG=${SCRIPT_DIR}/../../labust-visualization-pkg
+DES_PKG=${SCRIPT_DIR}/../../decision_making
 
-FOLDER_LIST="${LABUST_PKG} ${SENSOR_PKG} ${VIS_PKG} $*"
-PKG_LIST=${LABUST_LIST}${SENSOR_LIST}${VIS_LIST}
+FOLDER_LIST="${LABUST_PKG} ${SENSOR_PKG} ${VIS_PKG} ${DES_PKG} $*"
+PKG_LIST="${LABUST_LIST} ${SENSOR_LIST} ${VIS_LIST} ${DES_LIST}"
 CUR_FOLDER=`pwd`
 for folder in $FOLDER_LIST
 do
@@ -20,10 +22,13 @@ do
     DIRS=`find * -maxdepth 1 -type d`
     for dir in $DIRS
     do
-      if [[ $PKG_LIST =~ $dir ]]; then
-        echo "CATKIN_IGNORE directory $dir"
-        touch $dir/CATKIN_IGNORE
-      fi
+      for pkg in $PKG_LIST
+      do
+        if [[ $pkg == $dir ]]; then
+          echo "CATKIN_IGNORE directory $dir"
+          touch $dir/CATKIN_IGNORE
+        fi
+      done
     done
     cd $CUR_FOLDER
   fi
