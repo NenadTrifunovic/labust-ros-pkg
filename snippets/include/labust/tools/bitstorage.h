eb69cee8b45a3d2aeaaa7f224dbe7107f2f1a588
Dula Nad <dula.nad@gmail.com>
2016-08-21 14:07:47 +0200

    Added test cases for the bitstorage.

--- snippets/include/labust/tools/bitstorage.h
+++ snippets/include/labust/tools/bitstorage.h
@@ -34,150 +34,175 @@
 #ifndef SNIPPETS_BITSTORAGE_H
 #define SNIPPETS_BITSTORAGE_H
 
-#include <vector>
-#include <cassert>
-#include <cstdint>
 #include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdint>
+#include <vector>
 
 namespace labust
 {
-	namespace tools
-	{
-		struct BitStorage
-		{
-			enum {TOP_STORE=256};
+namespace tools
+{
+struct BitStorage
+{
+  enum
+  {
+    TOP_STORE = 256
+  };
 
-			static const uint64_t one = 1;
+  static const uint64_t one = 1;
 
-			BitStorage():_storage(0,0),bytept(0),bitpt(0)
-			{
-				_storage.reserve(TOP_STORE);
-			};
+  BitStorage() : _storage(0, 0), bytept(0), bitpt(0)
+  {
+    _storage.reserve(TOP_STORE);
+  };
 
-			BitStorage(const std::vector<uint8_t>& storage):_storage(storage),bytept(0),bitpt(0){};
+  BitStorage(const std::vector<uint8_t>& storage)
+    : _storage(storage), bytept(0), bitpt(0){};
 
-			///
-			template<class ValueType>
-			void put(const ValueType& data, double min, double max, uint8_t bitsz)
-			{
-				assert((min != max) && "Minimum and maximum should differ.");
-				assert((bitsz > 0) && "Bit size should be larger than zero.");
-				assert((min < max) && "Minimum should be smaller than maximum.");
+  ///
+  template <class ValueType>
+  void put(const ValueType& data, double min, double max, uint8_t bitsz)
+  {
+    assert((min != max) && "Minimum and maximum should differ.");
+    assert((bitsz > 0) && "Bit size should be larger than zero.");
+    assert((min < max) && "Minimum should be smaller than maximum.");
 
-				//Coerce to avoid overflow
-				ValueType rdata = data;
-				if (rdata > max) rdata = max; else if (rdata < min) rdata = min;
-				//Get the size mask (maximum 64 bits for data representation)
-				uint64_t mask = (one << bitsz)-one;
-				//Normalize data
-				uint64_t xw = uint64_t(round(mask * (rdata - min)/(max-min)));
-				xw &= mask;
-        //std::cout<<"\t Encoder number:"<<xw<<std::endl;
+    // Coerce to avoid overflow
+    ValueType rdata = data;
+    if (rdata > max)
+      rdata = max;
+    else if (rdata < min)
+      rdata = min;
+    // Get the size mask (maximum 64 bits for data representation)
+    uint64_t mask = (one << bitsz) - one;
+    // Normalize data
+    uint64_t xw = uint64_t(round(mask * (rdata - min) / (max - min)));
+    xw &= mask;
+    // std::cout << "\t Encoder number:" << xw << std::endl;
 
-				uint8_t rembits(bitsz);
-				while (rembits)
-				{
-          //introspect();
-          //std::cout<<"\t rembits:"<<int(rembits)<<std::endl;
-					//Check if enough size is available
-					if (_storage.size() <= bytept) _storage.push_back(0);
+    uint8_t rembits(bitsz);
+    while (rembits)
+    {
+      // introspect();
+      // std::cout<<"\t rembits:"<<int(rembits)<<std::endl;
+      // Check if enough size is available
+      if (_storage.size() <= bytept)
+        _storage.push_back(0);
 
-					int rem = 8-(bitpt);
-					if (rembits < rem) rem = rembits;
-					_storage[bytept] |= uint8_t((xw >> (rembits - rem)) << (bitpt));
-					bitpt += rem;
-          //std::cout<<"\t rem:"<<int(rem)<<std::endl;
-					if (bitpt == 8)
-					{
-						++(bytept);
-						bitpt = 0;
-					}
-					rembits -= rem;
-					xw &= (one << rembits) - one;
-				}
-        //introspect();
-			}
-
-			template<class ValueType>
-			bool get(ValueType& data, double min, double max, uint8_t bitsz)
-			{
-				assert((min != max) && "Minimum and maximum should differ.");
-				assert((bitsz > 0) && "Bit size should be larger than zero.");
-				assert((min < max) && "Minimum should be smaller than maximum.");
-
-				//Get the size mask (maximum 64 bits for data representation)
-				uint64_t mask = (one << bitsz)-one;
-				//Init data
-				uint64_t xw(0);
-
-				//Check if enough size is available
-				if (((_storage.size() - bytept)*8-bitpt) < bitsz) return false;
-
-				uint8_t rembits(bitsz);
-				while (rembits)
-				{
-					int rem = 8-(bitpt);
-					if (rembits < rem) rem = rembits;
-
-					xw |= ((_storage[bytept] & (((one << rem) - one) << bitpt)) >> bitpt) << (rembits-rem);
-
-					bitpt += rem;
-					if (bitpt == 8)
-					{
-						++(bytept);
-						bitpt = 0;
-					}
-					rembits -= rem;
-				}
-
-        //std::cout<<"\t Decoder number:"<<xw<<std::endl;
-
-				data = ValueType(((max-min)*xw)/mask + min);
-				return true;
-			}
-
-      /// Introspection into the storage
-      void introspect()
+      int rem = 8 - (bitpt);
+      if (rembits < rem)
       {
-          // Introspection
-          std::cout<<"BitStorage state"<<std::endl;
-          std::cout<<"\t Size:"<<_storage.size()<<std::endl;
-          std::cout<<"\t Bitpt:"<<int(bitpt)<<std::endl;
-          std::cout<<"\t Bytept:"<<bytept<<std::endl;
-          std::cout<<"\t Storage:";
-          for (int i=0; i<_storage.size(); ++i)
-          {
-            std::cout<<std::bitset<8>(_storage[i])<<" ";
-          }
-          std::cout<<std::endl;
-          usleep(2000*1000);
+        rem = rembits;
       }
+      _storage[bytept] |= uint8_t((xw >> (rembits - rem)) << (bitpt));
+      bitpt += rem;
+      // std::cout<<"\t rem:"<<int(rem)<<std::endl;
+      if (bitpt == 8)
+      {
+        ++(bytept);
+        bitpt = 0;
+      }
+      rembits -= rem;
+      xw &= (one << rembits) - one;
+    }
+    // introspect();
+  }
 
+  template <class ValueType>
+  bool get(ValueType& data, double min, double max, uint8_t bitsz)
+  {
+    assert((min != max) && "Minimum and maximum should differ.");
+    assert((bitsz > 0) && "Bit size should be larger than zero.");
+    assert((min < max) && "Minimum should be smaller than maximum.");
 
-			///Get storage
-			const std::vector<uint8_t>& storage() const {return _storage;}
+    // Get the size mask (maximum 64 bits for data representation)
+    uint64_t mask = (one << bitsz) - one;
+    // Init data
+    uint64_t xw(0);
 
-			///Reset storage
-			void reset(){bytept=0; bitpt=0;}
+    // Check if enough size is available
+    if (((_storage.size() - bytept) * 8 - bitpt) < bitsz)
+      return false;
 
-			///Clear storage
-			void clear(){_storage.clear();reset();}
+    uint8_t rembits(bitsz);
+    while (rembits)
+    {
+      int rem = 8 - (bitpt);
+      if (rembits < rem)
+        rem = rembits;
 
-			///Remaining bytes in storage
-			int remaining(){return _storage.size() - bytept;}
+      xw |= ((_storage[bytept] & (((one << rem) - one) << bitpt)) >> bitpt)
+            << (rembits - rem);
 
-		public:
-			//The continuous byte storage
-			std::vector<uint8_t> _storage;
-			//The byte pointer
-			uint32_t bytept;
-			//The bit pointer
-			uint32_t bitpt;
-		};
-	}
+      bitpt += rem;
+      if (bitpt == 8)
+      {
+        ++(bytept);
+        bitpt = 0;
+      }
+      rembits -= rem;
+    }
+
+    // std::cout<<"\t Decoder number:"<<xw<<std::endl;
+
+    data = ValueType(((max - min) * xw) / mask + min);
+    return true;
+  }
+
+  /// Introspection into the storage
+  void introspect()
+  {
+    // Introspection
+    std::cout << "BitStorage state" << std::endl;
+    std::cout << "\t Size:" << _storage.size() << std::endl;
+    std::cout << "\t Bitpt:" << int(bitpt) << std::endl;
+    std::cout << "\t Bytept:" << bytept << std::endl;
+    std::cout << "\t Storage:";
+    for (int i = 0; i < _storage.size(); ++i)
+    {
+      std::cout << std::bitset<8>(_storage[i]) << " ";
+    }
+    std::cout << std::endl;
+    usleep(2000 * 1000);
+  }
+
+  /// Get storage
+  const std::vector<uint8_t>& storage() const
+  {
+    return _storage;
+  }
+
+  /// Reset storage
+  void reset()
+  {
+    bytept = 0;
+    bitpt = 0;
+  }
+
+  /// Clear storage
+  void clear()
+  {
+    _storage.clear();
+    reset();
+  }
+
+  /// Remaining bytes in storage
+  int remaining()
+  {
+    return _storage.size() - bytept;
+  }
+
+public:
+  // The continuous byte storage
+  std::vector<uint8_t> _storage;
+  // The byte pointer
+  uint32_t bytept;
+  // The bit pointer
+  uint32_t bitpt;
+};
 }
-	/* SNIPPETS_BITSTORAGE_H */
+}
+/* SNIPPETS_BITSTORAGE_H */
 #endif
-
-
-
--- snippets/test/test_bitstorage.cpp
+++ snippets/test/test_bitstorage.cpp
@@ -1,12 +1,180 @@
 #include <gtest/gtest.h>
+#include <labust/tools/bitstorage.h>
+#include <cstdint>
 
-TEST(BitStorageTest, zeroInputbits)
+using labust::tools::BitStorage;
+
+class BitStorageTest : public ::testing::Test
 {
-  EXPECT_EQ(0, 0);
+protected:
+  template <class Type>
+  struct Element
+  {
+    typedef Type BaseType;
+    Element() : value(0), min(0), max(0), bitsz(0)
+    {
+    }
+
+    void set(Type value, double min, double max, int bitsz)
+    {
+      this->value = value;
+      this->min = min;
+      this->max = max;
+      this->bitsz = bitsz;
+    }
+
+    Type value;
+    double min;
+    double max;
+    int bitsz;
+  };
+
+  virtual void SetUp()
+  {
+    bitval.set(0, 0, 1, 1);
+    byteval.set(235, 0, 255, 8);
+    intval.set(645, 0, 1023, 10);
+    zdoubleval.set(0, -1, 1 - (2.0 / 512.0), 9);
+    nzdoubleval.set(0.75390625, -1, 1 - (2.0 / 512.0), 9);
+
+    bitval_encoded.push_back(bitval.value);
+    byteval_encoded.push_back(byteval.value);
+    intval_encoded.push_back(161);
+    intval_encoded.push_back(1);
+    zdoubleval_encoded.push_back(128);
+    zdoubleval_encoded.push_back(0);
+    nzdoubleval_encoded.push_back(224);
+    nzdoubleval_encoded.push_back(1);
+
+    multival_encoded.push_back(234);
+    multival_encoded.push_back(225);
+    multival_encoded.push_back(161);
+    multival_encoded.push_back(5);
+  }
+
+  virtual void TearDown()
+  {
+  }
+
+  template <class Type>
+  void testEncoding(BitStorage& storage, Element<Type> val,
+                    std::vector<uint8_t>& val_encoded)
+  {
+    storage.put(val.value, val.min, val.max, val.bitsz);
+    EXPECT_EQ(val_encoded.size(), storage.storage().size());
+
+    for (int i = 0; i < val_encoded.size(); ++i)
+      EXPECT_EQ(val_encoded[i], storage.storage()[i]);
+  }
+
+  template <class Type>
+  void testDecoding(BitStorage& storage, Element<Type> val)
+  {
+    Type decoded(-1);
+    storage.get(decoded, val.min, val.max, val.bitsz);
+    EXPECT_EQ(val.value, decoded);
+  }
+
+  Element<uint8_t> bitval;
+  Element<uint8_t> byteval;
+  Element<int> intval;
+  Element<double> zdoubleval;
+  Element<double> nzdoubleval;
+
+  std::vector<uint8_t> bitval_encoded;
+  std::vector<uint8_t> byteval_encoded;
+  std::vector<uint8_t> intval_encoded;
+  std::vector<uint8_t> zdoubleval_encoded;
+  std::vector<uint8_t> nzdoubleval_encoded;
+  std::vector<uint8_t> multival_encoded;
+};
+
+TEST_F(BitStorageTest, bitEncoding)
+{
+  BitStorage storage;
+  testEncoding(storage, bitval, bitval_encoded);
 }
 
-int main(int argc, char* argv[])
+TEST_F(BitStorageTest, byteEncoding)
 {
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  BitStorage storage;
+  testEncoding(storage, byteval, byteval_encoded);
+}
+
+TEST_F(BitStorageTest, intEncoding)
+{
+  BitStorage storage;
+  testEncoding(storage, intval, intval_encoded);
+}
+
+TEST_F(BitStorageTest, zeroDoubleEncoding)
+{
+  BitStorage storage;
+  testEncoding(storage, zdoubleval, zdoubleval_encoded);
+}
+
+TEST_F(BitStorageTest, nonzeroDoubleEncoding)
+{
+  BitStorage storage;
+  testEncoding(storage, nzdoubleval, nzdoubleval_encoded);
+}
+
+TEST_F(BitStorageTest, multiValueEncoding)
+{
+  BitStorage storage;
+  storage.put(bitval.value, bitval.min, bitval.max, bitval.bitsz);
+  storage.put(byteval.value, byteval.min, byteval.max, byteval.bitsz);
+  storage.put(nzdoubleval.value, nzdoubleval.min, nzdoubleval.max,
+              nzdoubleval.bitsz);
+  storage.put(intval.value, intval.min, intval.max, intval.bitsz);
+  EXPECT_EQ(multival_encoded.size(), storage.storage().size());
+
+  for (int i = 0; i < multival_encoded.size(); ++i)
+    EXPECT_EQ(multival_encoded[i], storage.storage()[i]);
+}
+
+TEST_F(BitStorageTest, bitDecoding)
+{
+  BitStorage storage(bitval_encoded);
+  testDecoding(storage, bitval);
+}
+
+TEST_F(BitStorageTest, byteDecoding)
+{
+  BitStorage storage(byteval_encoded);
+  testDecoding(storage, byteval);
+}
+
+TEST_F(BitStorageTest, intDecoding)
+{
+  BitStorage storage(intval_encoded);
+  testDecoding(storage, intval);
+}
+
+TEST_F(BitStorageTest, zeroDoubleDecoding)
+{
+  BitStorage storage(zdoubleval_encoded);
+  testDecoding(storage, zdoubleval);
+}
+
+TEST_F(BitStorageTest, nonZeroDoubleDecoding)
+{
+  BitStorage storage(nzdoubleval_encoded);
+  testDecoding(storage, nzdoubleval);
+}
+
+TEST_F(BitStorageTest, multiValueDecoding)
+{
+  BitStorage storage(multival_encoded);
+  testDecoding(storage, bitval);
+  testDecoding(storage, byteval);
+  testDecoding(storage, nzdoubleval);
+  testDecoding(storage, intval);
+}
+
+TEST_F(BitStorageTest, zeroInputbits)
+{
+  BitStorage storage;
+  storage.put(bitval.value, bitval.min, bitval.max, 0);
+  EXPECT_EQ(0, storage.storage().size()) << "Storage size not 0 bytes.";
 }
