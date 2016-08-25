2ffab9663d12a36ee7be5aa3085ac1bb63b48d2c
Buddy-san <labust.unizg@gmail.com>
2016-08-25 17:12:03 +0000

    Added zeroing for pressure.

--- labust_navigation/src/test/pressure2depth.cpp
+++ labust_navigation/src/test/pressure2depth.cpp
@@ -5,13 +5,27 @@
  *      Author: dnad
  */
 #include <std_msgs/Float32.h>
+#include <std_msgs/Bool.h>
 #include <ros/ros.h>
 
 double a(1.0),b(0.0);
+bool do_zero(false);
 ros::Publisher depthpub;
+
+void onZeroing(const std_msgs::Bool::ConstPtr& zero)
+{
+  do_zero = zero->data;
+}
 
 void onPressure(const std_msgs::Float32::ConstPtr& pressure)
 {
+  if (do_zero)
+  {
+     b = -a*pressure->data;
+     do_zero = false;
+     ROS_INFO("Pressure offset set to %f.", b);
+  }
+
   std_msgs::Float32 depth;
   depth.data = a*pressure->data + b;
   depthpub.publish(depth);
@@ -27,6 +41,7 @@ int main(int argc, char* argv[])
     ph.param("offset",b,b);
 
     ros::Subscriber pressure = nh.subscribe("pressure", 1, &onPressure);
+    ros::Subscriber reset = nh.subscribe("depth_reset", 1, &onZeroing);
     depthpub = nh.advertise<std_msgs::Float32>("depth",1);
 
     ros::spin();
