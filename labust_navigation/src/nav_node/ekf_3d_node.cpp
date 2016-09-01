2148a652b561b71d59535b74655099e21f562fe4
gibo1990 <filip.mandic@gmail.com>
2016-09-01 16:09:32 +0200

    Added new ekf filter node for testing.

---
+++ labust_navigation/src/nav_node/ekf_3d_node.cpp
@@ -0,0 +1,16 @@
+/*
+ * ekf_3d_node.cpp
+ *
+ *  Created on: Sep 1, 2016
+ *      Author: filip
+ */
+
+#include <labust/navigation/EKFModelBase.h>
+#include <labust/navigation/Estimator.h>
+
+#include <ros/ros.h>
+
+using namespace labust::navigation;
+
+
+
