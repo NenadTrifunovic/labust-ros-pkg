e0e363a248293b0a674a6316f2fbb244bc8e05f6
gibo1990 <filip.mandic@gmail.com>
2017-09-12 15:58:24 +0200

    Added source visualization marker.

--- labust_sim/src/data_sim/tdoa_sim.cpp
+++ labust_sim/src/data_sim/tdoa_sim.cpp
@@ -44,6 +44,7 @@
 #include <ros/ros.h>
 #include <std_msgs/Float64.h>
 #include <std_msgs/Time.h>
+#include <visualization_msgs/Marker.h>
 #include <Eigen/Dense>
 
 class TDOASim
@@ -67,6 +68,8 @@ public:
     pub_veh1_toa = nh.advertise<std_msgs::Time>("veh1/toa", 1);
     pub_veh2_toa = nh.advertise<std_msgs::Time>("veh2/toa", 1);
     pub_tdoa = nh.advertise<std_msgs::Float64>("tdoa", 1);
+
+    pub_vis = nh.advertise<visualization_msgs::Marker>("source_marker", 0);
   }
 
   ~TDOASim()
@@ -129,10 +132,36 @@ public:
   {
     source_pos << data->position.north, data->position.east,
         data->position.depth;
+
+    visualization_msgs::Marker marker;
+    marker.header.frame_id = "master/local";
+    marker.header.stamp = ros::Time();
+    marker.ns = "my_namespace";
+    marker.id = 0;
+    marker.type = visualization_msgs::Marker::CUBE;
+    marker.action = visualization_msgs::Marker::ADD;
+    marker.pose.position.x = data->position.north;
+    marker.pose.position.y = data->position.east;
+    marker.pose.position.z = data->position.depth;
+    marker.pose.orientation.x = 0.0;
+    marker.pose.orientation.y = 0.0;
+    marker.pose.orientation.z = 0.0;
+    marker.pose.orientation.w = 1.0;
+    marker.scale.x = 1;
+    marker.scale.y = 0.1;
+    marker.scale.z = 0.1;
+    marker.color.a = 1.0;  // Don't forget to set the alpha!
+    marker.color.r = 1.0;
+    marker.color.g = 0.0;
+    marker.color.b = 0.0;
+    // only if using a MESH_RESOURCE marker type:
+    // marker.mesh_resource =
+    // "package://pr2_description/meshes/base_v0/base.dae";
+    pub_vis.publish(marker);
   }
 
   ros::Subscriber sub_veh1_state, sub_veh2_state, sub_source_state;
-  ros::Publisher pub_veh1_toa, pub_veh2_toa, pub_tdoa;
+  ros::Publisher pub_veh1_toa, pub_veh2_toa, pub_tdoa, pub_vis;
 
   Eigen::Vector3d veh1_pos, veh2_pos, source_pos;
 
