71a7f25f29a9a7c37bf8af1906d9610f27393835
gibo1990 <filip.mandic@gmail.com>
2017-11-29 10:13:38 +0100

    Formattted code.

--- labust_navigation/include/labust/navigation/SensorHandlers.hpp
+++ labust_navigation/include/labust/navigation/SensorHandlers.hpp
@@ -1,7 +1,7 @@
 /*********************************************************************
  * Software License Agreement (BSD License)
  *
- *  Copyright (c) 2010, LABUST, UNIZG-FER
+ *  Copyright (c) 2017, LABUST, UNIZG-FER
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
@@ -35,15 +35,15 @@
 #define SENSORHANDLERS_HPP_
 #include <labust/math/NumberManipulation.hpp>
 
-#include <sensor_msgs/NavSatFix.h>
+#include <geometry_msgs/PointStamped.h>
+#include <geometry_msgs/TwistStamped.h>
 #include <sensor_msgs/Imu.h>
+#include <sensor_msgs/NavSatFix.h>
 #include <std_msgs/Bool.h>
 #include <std_msgs/Float64.h>
-#include <geometry_msgs/TwistStamped.h>
-#include <geometry_msgs/PointStamped.h>
-#include <underwater_msgs/USBLFix.h>
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
+#include <underwater_msgs/USBLFix.h>
 
 #include <GeographicLib/Geocentric.hpp>
 #include <GeographicLib/LocalCartesian.hpp>
@@ -56,179 +56,243 @@
 
 namespace labust
 {
-	namespace navigation
-	{
+namespace navigation
+{
+struct NewArrived
+{
+  NewArrived() : isNew(false){};
 
-		struct NewArrived
-		{
-			NewArrived():isNew(false){};
+  inline bool newArrived()
+  {
+    if (isNew)
+    {
+      isNew = false;
+      return true;
+    }
+    return false;
+  }
 
-			inline bool newArrived()
-			{
-				if (isNew)
-				{
-					isNew=false;
-					return true;
-				}
-				return false;
-			}
+protected:
+  bool isNew;
+};
+/**
+ * The local position handler.
+ */
+class LocalPosHandler : public NewArrived
+{
+public:
+  LocalPosHandler()
+    : listener(buffer)
+    , tf_prefix("")
+    , status_handler_("LocalPos", "localpos"){};
+  void configure(ros::NodeHandle& nh);
 
-		protected:
-			bool isNew;
-		};
-		/**
-		 * The local position handler.
-		 */
-		class LocalPosHandler : public NewArrived
-		{
-		public:
-			LocalPosHandler():listener(buffer),tf_prefix(""),status_handler_("LocalPos","localpos"){};
-			void configure(ros::NodeHandle& nh);
+  inline const std::pair<double, double>& position() const
+  {
+    return posxy;
+  }
 
-			inline const std::pair<double, double>&
-			position() const {return posxy;}
+private:
+  void onLocalPos(const geometry_msgs::PointStamped::ConstPtr& data);
+  std::pair<double, double> posxy;
+  ros::Subscriber localpos_sub;
+  std::string tf_prefix;
+  labust::diagnostic::StatusHandler status_handler_;
+  tf2_ros::Buffer buffer;
+  tf2_ros::TransformListener listener;
+};
+/**
+ * The GPS handler.
+ */
+class GPSHandler : public NewArrived
+{
+public:
+  GPSHandler()
+    : listener(buffer)
+    , originh(0)
+    , tf_prefix("")
+    , status_handler_("GPS", "gps"){};
+  void configure(ros::NodeHandle& nh);
 
-		private:
-			void onLocalPos(const geometry_msgs::PointStamped::ConstPtr& data);
-			std::pair<double, double> posxy;
-			ros::Subscriber localpos_sub;
-			std::string tf_prefix;
-			labust::diagnostic::StatusHandler status_handler_;
-			tf2_ros::Buffer buffer;
-			tf2_ros::TransformListener listener;
-		};
-		/**
-		 * The GPS handler.
-		 */
-		class GPSHandler : public NewArrived
-		{
-		public:
-			GPSHandler():listener(buffer),originh(0),tf_prefix(""),status_handler_("GPS","gps"){};
-			void configure(ros::NodeHandle& nh);
+  inline const std::pair<double, double>& position() const
+  {
+    return posxy;
+  }
 
-			inline const std::pair<double, double>&
-			position() const {return posxy;}
+  inline const std::pair<double, double>& origin() const
+  {
+    return originLL;
+  }
 
-			inline const std::pair<double, double>&
-			origin() const {return originLL;}
+  inline const double origin_h() const
+  {
+    return originh;
+  }
 
-			inline const double origin_h() const {return originh;}
+  inline const std::pair<double, double>& latlon() const
+  {
+    return posLL;
+  }
 
-			inline const std::pair<double, double>&
-			latlon() const	{return posLL;}
-			
-			void setRotation(const Eigen::Quaternion<double>& quat){this->rot = quat;};
+  void setRotation(const Eigen::Quaternion<double>& quat)
+  {
+    this->rot = quat;
+  };
 
-		private:
-			void onGps(const sensor_msgs::NavSatFix::ConstPtr& data);
-			std::pair<double, double> posxy, originLL, posLL;
-			tf2_ros::Buffer buffer;
-			tf2_ros::TransformListener listener;
-			ros::Subscriber gps;
-			//The ENU frame
-			GeographicLib::LocalCartesian proj;
-			Eigen::Quaternion<double> rot;
-			double originh;
-			std::string tf_prefix;
-			labust::diagnostic::StatusHandler status_handler_;
-		};
+private:
+  void onGps(const sensor_msgs::NavSatFix::ConstPtr& data);
+  std::pair<double, double> posxy, originLL, posLL;
+  tf2_ros::Buffer buffer;
+  tf2_ros::TransformListener listener;
+  ros::Subscriber gps;
+  // The ENU frame
+  GeographicLib::LocalCartesian proj;
+  Eigen::Quaternion<double> rot;
+  double originh;
+  std::string tf_prefix;
+  labust::diagnostic::StatusHandler status_handler_;
+};
 
-		/**
-		 * The imu handler.
-		 */
-		class ImuHandler : public NewArrived
-		{
-		public:
-			enum {roll=0, pitch, yaw};
-			enum {p=0,q,r};
-			enum {ax,ay,az};
+/**
+ * The imu handler.
+ */
+class ImuHandler : public NewArrived
+{
+public:
+  enum { roll = 0, pitch, yaw };
+  enum { p = 0, q, r };
+  enum { ax, ay, az };
 
-			ImuHandler():listener(buffer),gps(0),magdec(0),tf_prefix(""),status_handler_("IMU","imu"){};
-			
-			void setGpsHandler(GPSHandler* gps){this->gps = gps;};
+  ImuHandler()
+    : listener(buffer)
+    , gps(0)
+    , magdec(0)
+    , tf_prefix("")
+    , status_handler_("IMU", "imu"){};
 
-			void configure(ros::NodeHandle& nh);
+  void setGpsHandler(GPSHandler* gps)
+  {
+    this->gps = gps;
+  };
 
-			inline const double* orientation() const{return rpy;}
+  void configure(ros::NodeHandle& nh);
 
-			inline const double* rate() const{return pqr;}
+  inline const double* orientation() const
+  {
+    return rpy;
+  }
 
-			inline const double* acc() const{return axyz;}
+  inline const double* rate() const
+  {
+    return pqr;
+  }
 
-		private:
-			void onImu(const sensor_msgs::Imu::ConstPtr& data);
-			void onMagDec(const std_msgs::Float64::ConstPtr& data){magdec = data->data;}
-			tf2_ros::Buffer buffer;
-			tf2_ros::TransformListener listener;
-			labust::math::unwrap unwrap;
-			ros::Subscriber imu, mag_dec;
-			double rpy[3], pqr[3], axyz[3], magdec;
-			GPSHandler* gps;
-			std::string tf_prefix;
-			labust::diagnostic::StatusHandler status_handler_;
+  inline const double* acc() const
+  {
+    return axyz;
+  }
 
-		};
+private:
+  void onImu(const sensor_msgs::Imu::ConstPtr& data);
+  void onMagDec(const std_msgs::Float64::ConstPtr& data)
+  {
+    magdec = data->data;
+  }
+  tf2_ros::Buffer buffer;
+  tf2_ros::TransformListener listener;
+  labust::math::unwrap unwrap;
+  ros::Subscriber imu, mag_dec;
+  double rpy[3], pqr[3], axyz[3], magdec;
+  GPSHandler* gps;
+  std::string tf_prefix;
+  labust::diagnostic::StatusHandler status_handler_;
+};
 
-		class DvlHandler : public NewArrived
-		{
-		public:
-			enum {u=0,v,w};
+class DvlHandler : public NewArrived
+{
+public:
+  enum { u = 0, v, w };
 
-			DvlHandler():r(0),listener(buffer),bottom_lock(false),tf_prefix(""),status_handler_("DVL","dvl"){};
+  DvlHandler()
+    : r(0)
+    , listener(buffer)
+    , bottom_lock(false)
+    , tf_prefix("")
+    , status_handler_("DVL", "dvl"){};
 
-			void configure(ros::NodeHandle& nh);
+  void configure(ros::NodeHandle& nh);
 
-			inline const double* body_speeds() const {return uvw;};
+  inline const double* body_speeds() const
+  {
+    return uvw;
+  };
 
-			inline void current_r(double yaw_rate) {r = yaw_rate;};
+  inline void current_r(double yaw_rate)
+  {
+    r = yaw_rate;
+  };
 
-			inline bool has_bottom_lock() const {return bottom_lock;};
+  inline bool has_bottom_lock() const
+  {
+    return bottom_lock;
+  };
 
-		private:
-			void onDvl(const geometry_msgs::TwistStamped::ConstPtr& data);
-			void onBottomLock(const std_msgs::Bool::ConstPtr& data);
-			double uvw[3], r;
-			bool bottom_lock;
-			ros::Subscriber nu_dvl, dvl_bottom;
-			tf2_ros::Buffer buffer;
-			tf2_ros::TransformListener listener;
-			std::string tf_prefix;
-			labust::diagnostic::StatusHandler status_handler_;
-		};
+private:
+  void onDvl(const geometry_msgs::TwistStamped::ConstPtr& data);
+  void onBottomLock(const std_msgs::Bool::ConstPtr& data);
+  double uvw[3], r;
+  bool bottom_lock;
+  ros::Subscriber nu_dvl, dvl_bottom;
+  tf2_ros::Buffer buffer;
+  tf2_ros::TransformListener listener;
+  std::string tf_prefix;
+  labust::diagnostic::StatusHandler status_handler_;
+};
 
-		class iUSBLHandler : public NewArrived
-		{
-		public:
-			enum {x=0,y,z};
+class iUSBLHandler : public NewArrived
+{
+public:
+  enum { x = 0, y, z };
 
-			iUSBLHandler():depth(0),listener(buffer),tf_prefix(""),fix_arrived(false),status_handler_("iUSBL","iusbl"),
-					remote_arrived(false){};
+  iUSBLHandler()
+    : depth(0)
+    , listener(buffer)
+    , tf_prefix("")
+    , fix_arrived(false)
+    , status_handler_("iUSBL", "iusbl")
+    , remote_arrived(false){};
 
-			void configure(ros::NodeHandle& nh);
+  void configure(ros::NodeHandle& nh);
 
-			inline const double* position() const {return pos;};
+  inline const double* position() const
+  {
+    return pos;
+  };
 
-		private:
-			void onUSBL(const underwater_msgs::USBLFix::ConstPtr& data);
-			void onSurfacePos(const auv_msgs::NavSts::ConstPtr& data);
+private:
+  void onUSBL(const underwater_msgs::USBLFix::ConstPtr& data);
+  void onSurfacePos(const auv_msgs::NavSts::ConstPtr& data);
 
-			inline void current_z(double z) { depth = z;};
+  inline void current_z(double z)
+  {
+    depth = z;
+  };
 
-			void merge();
+  void merge();
 
-			double pos[3], depth;
+  double pos[3], depth;
 
-			ros::Subscriber usbl_sub, remote_pos_sub;
-			tf2_ros::Buffer buffer;
-			tf2_ros::TransformListener listener;
-			std::string tf_prefix;
-			auv_msgs::NavSts remote_position;
-			underwater_msgs::USBLFix fix;
-			bool fix_arrived;
-			bool remote_arrived;
-			labust::diagnostic::StatusHandler status_handler_;
-		};
-	}
+  ros::Subscriber usbl_sub, remote_pos_sub;
+  tf2_ros::Buffer buffer;
+  tf2_ros::TransformListener listener;
+  std::string tf_prefix;
+  auv_msgs::NavSts remote_position;
+  underwater_msgs::USBLFix fix;
+  bool fix_arrived;
+  bool remote_arrived;
+  labust::diagnostic::StatusHandler status_handler_;
+};
+}
 }
 /* SENSORHANDLERS_HPP_ */
 #endif
